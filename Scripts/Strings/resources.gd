cac44911a59364c8d606622817b40aa6fddb5f1e
César García <cesarg92e@gmail.com>
2024-04-15 16:24:26 +0200

    Modified resources.gd

--- Scripts/Strings/resources.gd
+++ Scripts/Strings/resources.gd
@@ -1,6 +1,5 @@
 extends Node
 class_name ResourceStrings
 
-const AUDIO_TEST_1: String = "res://Resources/Audio/test.tres"
-const AUDIO_TEST_2: String = "res://Resources/Audio/test_2.tres"
-const AUDIO_TEST_MEME: String = "res://Resources/Audio/test_meme.tres"
+# These are references to different resources. Add if necessary.
+#const AUDIO_TEST_1: String = "res://Resources/Audio/test.tres"
